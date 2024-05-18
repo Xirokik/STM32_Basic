998a9df2f8b111948ea5213713ab43cfe88887e8
Arek <137304484+Xirokik@users.noreply.github.com>
2024-05-18 12:46:34 +0200

    ADC_Config
    Without DMA

---
+++ P3_ADC/Inc/main.h
@@ -0,0 +1,14 @@
+/*
+ * main.h
+ *
+ *  Created on: Dec 20, 2023
+ *      Author: Arkadiusz
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdint.h>
+#include "stm32l4xx.h"
+
+#endif /* MAIN_H_ */
